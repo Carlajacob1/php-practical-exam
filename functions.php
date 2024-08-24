f0ba91cc88744e7229272238c8a32f56ed02b54d
CarlJacob1 <90488843+Carlajacob1@users.noreply.github.com>
2024-08-24 11:41:07 +0000

    Add form handling to greet users

--- functions.php
+++ functions.php
@@ -17,8 +17,24 @@ return "Hello, " .htmlspecialchars($name) . "!<br>Current date and time: " . $cu
 
 }
 
-
-
+//generate greeting using function
+echo generateGreeting($name);
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="name">Enter your name:</label>
+        <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>"
+        <input type="submit" value="Submit">
+        </form>
+</body>
+</html>
